9c330dc8f889981c55babf5cff1d640abbe8f97a
Chetan Fegade <cfegade@paloaltonetworks.com>
2022-05-08 22:18:08 -0700

    Added requirements.txt and updated get_pa_token with query params

--- cloudngfw_programmatic_access/get_pa_token.py
+++ cloudngfw_programmatic_access/get_pa_token.py
@@ -14,9 +14,10 @@ python get_pa_token.py \
 --get-token-url https://api.us-east-1.aws.cloudngfw.com/v1/mgmt/tokens/cloudfirewalladmin
 """
 import argparse
-import json
 import logging
 import requests
+import urllib3
+from urllib3.exceptions import InsecureRequestWarning
 
 from auth_aws_iam import AuthAwsIam
 from pa_utils import (
@@ -25,7 +26,26 @@ from pa_utils import (
 )
 
 
+def get_request_url(url, query_params_dict=None):
+    pa_logger.debug(f"URL: {url}")
+    pa_logger.debug(f"QUERY PARAMS: {query_params_dict}")
+    request_url = url
+
+    if query_params_dict is not None:
+        request_url = f"{request_url}?"
+        for key, value in query_params_dict.items():
+            request_url = f"{request_url}{key}={value}&"
+        request_url = request_url[:-1]
+
+    pa_logger.debug(f"Request URL: {request_url}")
+    return request_url
+
+
 if __name__ == "__main__":
+    # Setup Environment
+    urllib3.disable_warnings(InsecureRequestWarning)
+
+    # Parse Arguments
     parser = argparse.ArgumentParser(
         description="Parameters need to get programmatic access token for Cloud NGFW.",
         formatter_class=argparse.RawTextHelpFormatter,
@@ -46,14 +66,14 @@ if __name__ == "__main__":
                         help="Specify the aws profile which can assume the role arn",
                         )
     parser.add_argument("--role-arn",
-                        dest="rolearn",
+                        dest="role_arn",
                         required=True,
                         metavar="ROLE_ARN",
                         type=str,
                         help="Specify the role arn with Cloud NGFW programmatic access tags",
                         )
     parser.add_argument("--get-token-url",
-                        dest="gettokenurl",
+                        dest="get_token_url",
                         required=True,
                         metavar="GET_TOKEN_URL",
                         type=str,
@@ -69,35 +89,54 @@ if __name__ == "__main__":
     args = parser.parse_args()
     pa_logger.setLevel(logging.DEBUG if args.debug else logging.INFO)
 
-    pa_logger.info(f"ARGS: {args}")
+    pa_logger.debug(f"ARGS: {args}")
     pa_logger.debug(f"Region: {args.region}")
     pa_logger.debug(f"Profile: {args.profile}")
-    pa_logger.debug(f"RoleArn: {args.rolearn}")
-    pa_logger.debug(f"GetTokenUrl: {args.gettokenurl}")
+    pa_logger.debug(f"RoleArn: {args.role_arn}")
+    pa_logger.debug(f"GetTokenUrl: {args.get_token_url}")
 
+    # Get AWS IAM temporary credentials
     _auth_iam = AuthAwsIam(
         region=args.region,
-        role_to_assume=args.rolearn,
+        role_to_assume=args.role_arn,
         profile_to_assume=args.profile,
     )
     pa_logger.debug(f"AccessKey: {_auth_iam.access_key}")
     pa_logger.debug(f"SecretKey: {_auth_iam.secret_key}")
     pa_logger.debug(f"SessionToken: {_auth_iam.session_token}")
 
-    body = {
-        "ExpiryTime": 60
+    # Request Body
+    body_dict = {}
+
+    # Request Query Params
+    query_params_dict = {
+        "expirytime": 60,
     }
 
-    request_url = args.gettokenurl
+    # Headers
+    sign_url = get_request_url(
+        url=args.get_token_url,
+        query_params_dict=query_params_dict,
+    )
 
-    headers = _auth_iam.get_headers(HTTP_VERB_GET, request_url, body)
+    headers = _auth_iam.get_headers(HTTP_VERB_GET, sign_url, body_dict)
 
-    session = requests.session()
-    response = requests.get(request_url, headers=headers, data=json.dumps(body), verify=False)
+    # Request
+    response = requests.get(
+        url=args.get_token_url,
+        headers=headers,
+        json=body_dict,
+        params=query_params_dict,
+        verify=False,
+    )
+    pa_logger.debug(f"ResponseUrl: {response.url}")
+    pa_logger.debug(f"ResponseStatusCode: {response.status_code}")
     pa_logger.debug(f"ResponseText: {response.text}")
 
+    # Parse response
     resp_dict = response.json()
     assert resp_dict['ResponseStatus']['ErrorCode'] == 0
 
+    # Output
     pa_logger.info(f"Cloud NGFW Programmatic Access Token: {resp_dict['Response']['TokenId']}")
     pa_logger.info(f"Cloud NGFW Subscription Key: {resp_dict['Response']['SubscriptionKey']}")
